140ea70d1166b08a17176e7fe29e3424adb286ae
Colin Cameron <ccameron2020@gmail.com>
2022-10-16 21:40:59 +0100

    made fancier

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import MovieContainer from './containers/MovieContainer';
 
 function App() {
+  const footer = require('./images/footer.png')
   return (
 <>
     <header> <img src={logo} className="App-logo" alt="logo" /></header>
     <div className="App">
       <MovieContainer/>
     </div>
+    <footer><img className='footer' src={footer} /></footer>
 </>
   );
 }
